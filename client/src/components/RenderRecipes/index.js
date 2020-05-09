8c079406a0ac20ee6c14094c14cda5ba94838bc8
Minroe <scott.whitney334@gmail.com>
2020-05-09 16:38:09 -0700

    Recipe cards

--- client/src/components/RenderRecipes/index.js
+++ client/src/components/RenderRecipes/index.js
@@ -1,32 +1,45 @@
 import React, { Component } from "react";
 import axios from "axios";
+import "./style.css";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import RecipeCard from "../RecipeCard";
 const api_Key = "fa3502941336b2865937f7efea9a0b60";
 const api_ID = "3aaa0d6e";
 class Recipes extends Component {
   state = {
     recipeTitle: "",
     recipeDetail: {},
+    recipeTwoDetail: {},
+    recipeThreeDetail: {},
     gotRecipe: false,
   };
   //Define onclick method to display recipes
-  getRecipe = async () => {
-    // event.preventDefault();
-    console.log(api_Key , api_ID)
+  getRecipe = async event => {
+    event.preventDefault();
+   
+    console.log(api_Key, api_ID);
     try {
       const { data } = await axios.get(
         `https://api.edamam.com/search?q=${this.state.recipeTitle}&app_id=${api_ID}&app_key=${api_Key}`
       );
-      // const body = JSON.stringify({
-      //      title: data.get('RecipeTitle'),
-      //      content: data.get('recipeDetail'),
-      //  });
-      console.log('hello');
+      console.log("hello");
       console.log(data.hits);
+      
       this.setState({
         recipeDetail: data.hits[0],
-        gotRecipe: true
+        recipeTwoDetail: data.hits[1],
+        recipeThreeDetail: data.hits[2],
+        gotRecipe: true,
       });
-      console.log("Recipe",this.state.gotRecipe, this.state.recipeDetail )
+      
+      console.log(
+        "Recipe",
+        this.state.gotRecipe,
+        this.state.recipeDetail,
+        this.state.recipeTwoDetail
+      );
     } catch (e) {
       console.log(e);
     }
@@ -44,39 +57,70 @@ class Recipes extends Component {
   // }
   render() {
     return (
-      <div className="mt-4">
-        <div>
-          <div id="app" className="container">
-            
-              {/* <button className="btn btn-lg btn-primary btn-block" onClick={this.signUp} type="button">Sign up</button>  */}
-            <input
-              type="text"
-              name="recipeTitle"
-              value={this.state.recipeTitle}
-              onChange={this.handleInput}
-            />
-            <button
-              className="btn btn-lg btn-primary btn-block"
-              onClick={this.getRecipe}
-              type="button">Get Recipes</button>
-          </div>
-          {/* <img src={recipe.image_url} alt={recipe.title} /> */}
-        </div>
-        <div>
-          {/* <Link to="/">{"RenderRecipes"}</Link> */}
-          {/* <h4>{this.state.recipeTitle}</h4> */}
-          {/* <img src={} /> */}
-          {this.state.gotRecipe ? (
-            <div>
-              <h4>{this.state.recipeDetail.recipe.label}</h4>
-              <img src={this.state.recipeDetail.recipe.image} alt={this.state.recipeDetail.recipe.labe} />
-              <a href={this.state.recipeDetail.recipe.url} target="_blank" rel="noopener noreferrer">{this.state.recipeDetail.recipe.url}</a>
+      <Container>
+        <Row>
+          <Col lg="4">
+            <div className="card delta">
+              <div className="card-body">
+                <h5 className="card-title">Recipe Finder</h5>
+                <h6 className="card-subtitle mb-2 text-muted">
+                  For when you don't know what to make of it.
+                </h6>
+                <form>
+                  <input
+                    type="text"
+                    name="recipeTitle"
+                    value={this.state.recipeTitle}
+                    onChange={this.handleInput}
+                  />
+                  <button
+                    className="btn btn-lg btn-primary btn-block"
+                    onClick={this.getRecipe}
+                    type="button"
+                  >
+                    Get Recipes
+                  </button>
+                </form>
+                <p>
+                  Type in an ingredient or the name something you have in your
+                  fridge
+                </p>
+              </div>
             </div>
-          ): 
-          (<h3>No results display!!</h3>)
-          }
-        </div>
-      </div>
+          </Col>
+
+          <Col md="auto">
+            {this.state.gotRecipe ? (
+              <RecipeCard
+                image={this.state.recipeDetail.recipe.image}
+                address={this.state.recipeDetail.recipe.url}
+                food={this.state.recipeDetail.recipe.label}
+                type={this.state.recipeDetail.recipe.totalTime}
+              />
+            ) : null}
+          </Col>
+          <Col md="auto">
+            {this.state.gotRecipe ? (
+              <RecipeCard
+                image={this.state.recipeTwoDetail.recipe.image}
+                address={this.state.recipeTwoDetail.recipe.url}
+                food={this.state.recipeTwoDetail.recipe.label}
+                type={this.state.recipeTwoDetail.recipe.totalTime}
+              />
+            ) : null}
+          </Col>
+          <Col md="auto">
+            {this.state.gotRecipe ? (
+              <RecipeCard
+                image={this.state.recipeThreeDetail.recipe.image}
+                address={this.state.recipeThreeDetail.recipe.url}
+                food={this.state.recipeThreeDetail.recipe.label}
+                type={this.state.recipeThreeDetail.recipe.totalTime}
+              />
+            ) : null}
+          </Col>
+        </Row>
+      </Container>
     );
   }
 }
