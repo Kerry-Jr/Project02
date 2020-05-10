b887d38aff68543d0ba69ac7afc59cc41f504dd5
Kerry-Jr <kerrysfs@gmail.com>
2020-05-10 14:23:40 -0700

    fixed navbar link, deleted opacity on hero-image, updated name

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -8,7 +8,7 @@ const Navbar = () => (
       <div className='row no-gutters'>
         <div className='col-12'>
           <nav className="navbar navbar-expand-lg navbar-dark bg-light fixed-top">
-            <Link className="navbar-brand" to="/">????</Link>
+            <Link className="navbar-brand" to="/">The Ctrl-Alt-Delete club </Link>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
             </button>
@@ -18,7 +18,7 @@ const Navbar = () => (
                   <Link className="nav-link" to="/">Home <span className="sr-only">(current)</span></Link>
                 </li>
                 <li className="nav-item">
-                  <Link className="nav-link" to="./">Latest Blog/Post</Link>
+                  <Link className="nav-link" to="/view">Latest Blog/Post</Link>
                 </li>
                 <li className="nav-item">
                   <Link className="nav-link" to="/renderRecipes">Recipe Search</Link>
